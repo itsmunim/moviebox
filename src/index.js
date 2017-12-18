3ebe55884230fafa17378c25305f2c8a81357829
dibosh <dibosh@newscred.com>
2017-12-19 00:34:29 +0600

    Turn anonymous functions into arrow functions   - Define routes entrypoint

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 let app = express();
+let routes = require('./routes/index');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('combined'));
+app.use('/', routes);
 
 let port = process.env.PORT || 8080;
 
@@ -20,35 +22,33 @@ let connections = [];
 process.on('SIGTERM', shutDown);
 process.on('SIGINT', shutDown);
 
-server.on('connection', function (connection) {
+server.on('connection', (connection) => {
   connections.push(connection);
   console.log('%s connections currently open', connections.length);
   connection.on('close', function () {
-    connections = connections.filter(function (curr) {
-      return curr !== connection;
-    });
+    connections = connections.filter((curr) => { return curr !== connection; });
   });
 });
 
 function shutDown() {
   console.log('Received kill signal, shutting down gracefully');
 
-  server.close(function () {
+  server.close(() => {
     console.log('Closed out remaining connections');
     process.exit(0);
   });
 
-  connections.forEach(function (curr) {
+  connections.forEach((curr) => {
     curr.end();
   });
 
-  setTimeout(function () {
-    connections.forEach(function (curr) {
+  setTimeout(() => {
+    connections.forEach((curr) => {
       curr.destroy();
     });
   }, 5000);
 
-  setTimeout(function () {
+  setTimeout(() => {
     console.error('Could not close connections in time, forcefully shutting down');
     process.exit(1);
   }, 10000);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var router = express.Router();
 /**
  All the routes defined in this dir will be referenced here.
  */
+module.exports = router;
