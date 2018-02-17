cda5daf6cf4688497f556818f5e0ea9f78469936
dibosh <dibosh@newscred.com>
2018-02-17 19:22:23 +0600

    Sceffold client app

---
+++ client/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <h1>Moviebox</h1>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
