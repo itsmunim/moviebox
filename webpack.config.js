3a07cb1a333fdc19757bc5242dd3e19345628ff6
dibosh <dibosh@newscred.com>
2018-02-22 00:55:17 +0600

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ var config = {
         test : /\.jsx?/,
         include : APP_DIR,
         loader : 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
       }
     ]
   }
