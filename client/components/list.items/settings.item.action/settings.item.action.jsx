d222cde0d94b5b1b6229b7b4e5c04b8f6e909209
dibosh <dibosh@newscred.com>
2018-03-10 18:43:06 +0600

    Pass event on action click fire

--- client/components/list.items/settings.item.action/settings.item.action.jsx
+++ client/components/list.items/settings.item.action/settings.item.action.jsx
@@ -19,7 +19,7 @@ class SettingsItemAction extends React.Component {
   render() {
     return (
       <li className="settings-item-action">
-        <button onClick={() => this.item.callback() }
+        <button onClick={(event) => this.item.callback(event) }
                 className={this.getActionClassNames(this.item.type)}>
           {this.item.title}
         </button>
