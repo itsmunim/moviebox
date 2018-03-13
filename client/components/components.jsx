402af828b583cd4e3fe1e88fae28dd086601feb7
dibosh <dibosh@newscred.com>
2018-03-14 00:26:51 +0600

    Expose experimental file explorer component   - uses fake file data

--- client/components/components.jsx
+++ client/components/components.jsx
@@ -2,5 +2,6 @@ import BasicList from './basic.list/basic.list.jsx';
 import SettingsItem from './list.items/settings.item/settings.item.jsx';
 import SettingsItemAction from './list.items/settings.item.action/settings.item.action.jsx';
 import Modal from './modal/modal.jsx';
+import FileExplorer from './file.explorer/file.explorer.jsx';
 
-export {BasicList, SettingsItem, SettingsItemAction, Modal};
+export {BasicList, SettingsItem, SettingsItemAction, Modal, FileExplorer};
