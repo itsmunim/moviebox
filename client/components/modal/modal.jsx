0f7b4a424b3dc28d2c058146ef6879d4fb27840e
dibosh <dibosh@newscred.com>
2018-03-14 00:45:19 +0600

    Simplify modal visibility

--- client/components/modal/modal.jsx
+++ client/components/modal/modal.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import classNames from 'classnames';
 
 class Modal extends React.Component {
   constructor(props) {
     super(props);
-    this.modalBackdropStyle = {
-      backgroundColor: 'rgba(0,0,0,0.5)'
-    };
   }
 
-  modalClasses() {
-    return classNames({
-      'modal': true,
-      'd-block': this.props.isVisible
-    });
+  getComponentClasses() {
+    let classes = ['modal', 'fade'];
+    if (this.props.isVisible) {
+      classes.push('show');
+      classes.push('backdrop-visible');
+    }
+    return classes.join(' ');
   }
 
   render() {
     return (
-      <div className={this.modalClasses()} style={this.modalBackdropStyle} onClick={() => this.props.onModalClose()}>
+      <div className={this.getComponentClasses()} onClick={() => this.props.onModalClose()}>
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
