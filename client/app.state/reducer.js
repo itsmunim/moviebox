e2811eeefc9b8b23274ceeb590d68e9d84750aef
dibosh <dibosh@newscred.com>
2018-03-19 22:44:26 +0600

    Add a new state for file explorer component and update reducer function

--- client/app.state/reducer.js
+++ client/app.state/reducer.js
@@ -1,10 +1,13 @@
 import HeaderState from './states/header';
 import SettingsPageState from './states/settings';
+import FileExplorerState from './states/file.explorer';
 
 const stateReducer = (previousState, action) => {
   let newState = Object.assign({}, previousState);
   newState.header = HeaderState.update(newState.header, action);
   newState.settingsPage = SettingsPageState.update(newState.settingsPage, action);
+  newState.components = newState.components || {};
+  newState.components.fileExplorer = FileExplorerState.update(newState.components.fileExplorer, action);
   return newState;
 };
 
