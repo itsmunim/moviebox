34a6c63f234cbc5daf4f5141736ef12aeb0f33b8
dibosh <dibosh@newscred.com>
2018-03-19 22:48:21 +0600

    FileExplorerService::fetchFilesInPath -> Return all files not just folders

--- client/common/services/file.explorer.service.js
+++ client/common/services/file.explorer.service.js
@@ -1,13 +1,10 @@
-import _ from 'lodash';
 import BaseHttpService from './base.http.service';
 
 class FileExplorerService extends BaseHttpService {
-  static fetchFoldersInPath(path) {
+  static fetchFilesInPath(path) {
     return FileExplorerService.makeGETCall('/explorer/files', null, {path})
       .then((files) => {
-        return _.filter(files, (file) => {
-          return file.isDirectory;
-        });
+        return files;
       });
   }
 }
