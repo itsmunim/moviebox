70946a92bc6de3420dd14224a517411739160cce
dibosh <dibosh@newscred.com>
2018-03-23 14:23:18 +0600

    Add redux logger

--- client/app.state/store.js
+++ client/app.state/store.js
@@ -1,8 +1,13 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger';
 import stateReducer from './reducer';
 
-const store = createStore(stateReducer, {
-  components: {},
-  pages: {}
-});
+const store = createStore(
+  stateReducer,
+  {
+    components: {},
+    pages: {}
+  },
+  applyMiddleware(logger)
+);
 export default store;
