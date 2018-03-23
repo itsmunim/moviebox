a2d2f7f5ce04a4ab25325f99de5cabac690bf9dc
dibosh <dibosh@newscred.com>
2018-03-23 18:06:07 +0600

    Return only movie files or folders

--- client/common/services/file.explorer.service.js
+++ client/common/services/file.explorer.service.js
@@ -1,10 +1,17 @@
 import BaseHttpService from './base.http.service';
+import _ from 'lodash';
 
 class FileExplorerService extends BaseHttpService {
+  static getAllowedExtensions() {
+    return ['mkv', 'mp4', 'avi'];
+  }
+
   static fetchFilesInPath(path) {
     return FileExplorerService.makeGETCall('/explorer/files', null, {path})
       .then((files) => {
-        return files;
+        return _.filter(files, (file) => {
+          return file.isDirectory || _.includes(FileExplorerService.getAllowedExtensions(), file.extension);
+        });
       });
   }
 }
