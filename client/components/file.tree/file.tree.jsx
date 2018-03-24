413d364f79df0ea61d061ed79624f628118fec84
dibosh <dibosh@newscred.com>
2018-03-24 21:44:47 +0600

    Wrap file items with event handlers while rendering using basic list

--- client/components/file.tree/file.tree.jsx
+++ client/components/file.tree/file.tree.jsx
@@ -4,10 +4,45 @@ import FileItem from '../list.items/file.item/file.item.jsx';
 import BasicList from '../basic.list/basic.list.jsx';
 import Loader from '../loader/loader.jsx';
 import ConditionalContainer from '../conditional.container/conditional.container.jsx';
+import * as DataStructures from '../../common/classes/index';
 
 class FileTree extends React.Component {
   isFileTreeLoaded() {
     return !_.isEmpty(this.props.fileTree);
+  }
+
+  openFolder(fileItem) {
+    let fileTreeCopy = DataStructures.FileTree.clone(this.props.fileTree);
+    fileTreeCopy.currentNode = fileItem;
+    this.props.onFileTreeUpdate(fileTreeCopy);
+  }
+
+  getCurrentPathNavigation() {
+    if (!this.props.fileTree) { return; }
+    let traversals = DataStructures.FileTree.getPathTraversalFromCurrentNode(this.props.fileTree);
+    return traversals.map((file, index) => {
+      let isLast = index === traversals.length - 1;
+      let fileTraversalPathComponent = file.isDirectory && !isLast
+        ? <span className="text-primary" onClick={() => this.openFolder(file)}>{file.name}</span>
+        : <span className="text-secondary">{file.name}</span>;
+
+      return <span key={file.path}><span>/</span>{fileTraversalPathComponent}</span>;
+    });
+  }
+
+  getFileItemsWithEventHandlers() {
+    return _.map(_.get(this.props.fileTree, 'currentNode.files', []), (fileItem) => {
+      if (fileItem.isDirectory) {
+        fileItem.open = () => {
+          this.openFolder(fileItem);
+        };
+        fileItem.select = () => {
+          this.props.onFolderChoose(fileItem);
+        };
+      }
+
+      return fileItem;
+    });
   }
 
   render() {
@@ -18,9 +53,9 @@ class FileTree extends React.Component {
         </ConditionalContainer>
 
         <ConditionalContainer if={this.isFileTreeLoaded()}>
-          <h5 className="current-path">
-            {_.get(this.props.fileTree, 'currentNode.path', '')}
-          </h5>
+          <p className="current-path">
+            {this.getCurrentPathNavigation()}
+          </p>
           <p className="current-folder">
             <i className="fas fa-folder-open"></i>
             <span>
@@ -28,8 +63,9 @@ class FileTree extends React.Component {
             </span>
           </p>
           <div className="files">
-            <BasicList listClass={'file-list max-h-400 y-scrollable'} itemComponent={FileItem}
-                       items={_.get(this.props.fileTree, 'currentNode.files', [])}/>
+            <BasicList listClass={'file-list max-h-400 y-scrollable'}
+                       itemComponent={FileItem}
+                       items={this.getFileItemsWithEventHandlers()}/>
           </div>
         </ConditionalContainer>
       </div>
