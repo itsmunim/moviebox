a2b9683ca3c98a739f0f4e267310a3ff85e1cdc0
dibosh <dibosh@newscred.com>
2018-03-24 22:57:12 +0600

    Remove unnecessary constructor from settings item component

--- client/components/list.items/settings.item/settings.item.jsx
+++ client/components/list.items/settings.item/settings.item.jsx
@@ -3,20 +3,17 @@ import BasicList from '../../basic.list/basic.list.jsx';
 import SettingsItemAction from '../settings.item.action/settings.item.action.jsx';
 
 class SettingsItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.item = props.item;
-  }
   render() {
     return (
       <li className="settings-item">
         <div className="settings-item-wrapper d-flex flex-row">
           <div className="mr-auto">
-            <h5>{this.item.title}</h5>
-            <p>{this.item.subtitle}</p>
+            <h5>{this.props.item.title}</h5>
+            <p>{this.props.item.subtitle}</p>
           </div>
           <div>
-            <BasicList listClass={'settings-item-actions'} itemComponent={SettingsItemAction} items={this.item.actions}/>
+            <BasicList listClass={'settings-item-actions'} itemComponent={SettingsItemAction}
+                       items={this.props.item.actions}/>
           </div>
         </div>
       </li>
