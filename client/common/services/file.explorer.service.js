ec2e248f1d8193d8da1e52d399a595fa80221a21
dibosh <dibosh@newscred.com>
2018-03-24 18:13:22 +0600

    Make filtering of files based on extension working

--- client/common/services/file.explorer.service.js
+++ client/common/services/file.explorer.service.js
@@ -3,14 +3,14 @@ import _ from 'lodash';
 
 class FileExplorerService extends BaseHttpService {
   static getAllowedExtensions() {
-    return ['mkv', 'mp4', 'avi'];
+    return ['.mkv', '.mp4', '.avi'];
   }
 
   static fetchFilesInPath(path) {
-    return FileExplorerService.makeGETCall('/explorer/files', null, {path})
+    return FileExplorerService.makeGETCall('/explorer/files', {path})
       .then((files) => {
         return _.filter(files, (file) => {
-          return file.isDirectory || _.includes(FileExplorerService.getAllowedExtensions(), file.extension);
+          return file.isDirectory || _.includes(FileExplorerService.getAllowedExtensions(), file.ext);
         });
       });
   }
