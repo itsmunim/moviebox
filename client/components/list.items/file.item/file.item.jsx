b2866bd0d66471a78457b90f1b792e36ce2daaa6
dibosh <dibosh@newscred.com>
2018-03-24 21:33:03 +0600

    Change how file items are displayed

--- client/components/list.items/file.item/file.item.jsx
+++ client/components/list.items/file.item/file.item.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
+import ConditionalContainer from '../../conditional.container/conditional.container.jsx';
 
 class FileItem extends React.Component {
   getIconClass() {
     return 'fas ' + (this.props.item.isDirectory ? 'fa-folder' : 'fa-film');
   }
 
+  getFileItemClass() {
+    return 'file-item ' + (this.props.item.isDirectory ? 'folder-type' : 'file-type');
+  }
+
+  getFileNameClass() {
+    return 'file-item-name ' + (this.props.item.isDirectory ? 'folder-type' : 'file-type');
+  }
+
   render() {
     return (
-      <li className="file-item">
-        <p>
-          <span className="icon-holder">
-            <i className={this.getIconClass()}></i>
-          </span>
-          <span>{this.props.item.name}</span>
-        </p>
+      <li className={this.getFileItemClass()}
+          onDoubleClick={() => this.props.item.open(this.props.item)}>
+        <div className="d-flex flex-row justify-content-center">
+          <div className="mr-auto">
+            <p className={this.getFileNameClass()}>
+              <span className="icon-holder">
+                <i className={this.getIconClass()}></i>
+              </span>
+              <span>{this.props.item.name}</span>
+            </p>
+            <ConditionalContainer cType={'p'} cls={'help-text'} if={this.props.item.isDirectory}>
+              <small>Double click to go inside</small>
+            </ConditionalContainer>
+          </div>
+
+          <ConditionalContainer cls={'mr-auto-0'} if={this.props.item.isDirectory}>
+            <a href="#" className="btn-select-folder text-primary"
+               onClick={() => this.props.item.select(this.props.item)}>
+              Select
+            </a>
+          </ConditionalContainer>
+        </div>
       </li>
     );
   }
