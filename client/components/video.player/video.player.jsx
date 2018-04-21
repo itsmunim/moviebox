21125eca709964c20ab46fafb1a573229fae0cf2
rubayeet <rubayeet.islam@gmail.com>
2018-04-21 11:25:17 -0300

    Cosmetic changes

--- client/components/video.player/video.player.jsx
+++ client/components/video.player/video.player.jsx
@@ -4,22 +4,16 @@ import 'video.js/dist/video-js.css';
 
 class VideoPlayer extends React.Component {
   componentDidMount() {
-    // instantiate Video.js
     this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-    //   console.log('onPlayerReady', this)
     });
   }
 
-  // destroy player on unmount
   componentWillUnmount() {
     if (this.player) {
       this.player.dispose();
     }
   }
 
-  // wrap the player in a div with a `data-vjs-player` attribute
-  // so videojs won't create additional wrapper in the DOM
-  // see https://github.com/videojs/video.js/pull/3856
   render() {
     return (
       <div>
